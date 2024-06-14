0a551a9cd281c859db3e7c6cfeabc4b7ee130d99
juliarobaina <julia.robaina8@gmail.com>
2024-06-14 01:09:37 -0300

    solução mais simples

---
+++ nomeAlturaIdade_1.py
@@ -0,0 +1,5 @@
+nomeUsuario = input("Digite seu nome: ")
+altura = float(input("Digite sua altura: "))
+idade = int(input("Digite sua idade: "))
+
+print(f"Seu nome é {nomeUsuario}, sua altura é {altura}m e você tem {idade} anos.")
