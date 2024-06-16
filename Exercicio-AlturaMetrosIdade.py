4d2380088db96ab0ed9e0e5929a6f1ef325d9ae9
Vitor Back <vitor.back10@outlook.com>
2024-06-15 20:28:58 -0400

    Exercicio de segunda feira

---
+++ Exercicio-AlturaMetrosIdade.py
@@ -0,0 +1,5 @@
+nome= input("Informe seu nome:")
+altua= float(input("Informe sua altura:"))
+idade= int(input("Informe sua idade:"))
+
+print(f"Seu nome é: {nome}\nSua altura é: {altua}\nSua idade é: {idade}")
