f91610880948159bcd684687365d177f48330489
Caio <caiobarreto00@gmail.com>
2024-06-17 11:07:10 -0300

    Atividade segunda-feira

---
+++ ativ_ida_alt_name.py
@@ -0,0 +1,5 @@
+nome = input('Digite seu nome: ')
+altura = float(input('Digite sua altura: '))
+idade = int(input('Digite sua idade: '))
+
+print('Seu nome é:', nome  + ',' +  ' sua altura é:',altura, 'sua idade é:',idade)
