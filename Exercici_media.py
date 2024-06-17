de0a4ba0cd3f54784593fd6568b1cf80c58a2b22
Vitor Back <vitor.back10@outlook.com>
2024-06-17 19:33:24 -0400

    Atualização media

--- Exercici_media.py
+++ Exercici_media.py
@@ -1,8 +1,9 @@
-nota1= float(input("Informe nota1:"))
-nota2= float(input("Informe nota2:"))
-nota3= float(input("Informe nota3:"))
-nota4= float(input("Informe nota4:"))
+Nome = (input("informe o nome:"))
+nota1= float(input("Informe nota 1:"))
+nota2= float(input("Informe nota 2:"))
+nota3= float(input("Informe nota 3:"))
+nota4= float(input("Informe nota 4:"))
 media = (nota1 + nota2 + nota3 + nota4) / 4
 
-print(f"A media é: {media}")
+print(f"Olá, {Nome}! Sua média é: {media} pontos")
 
