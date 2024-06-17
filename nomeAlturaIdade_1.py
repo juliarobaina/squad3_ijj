a17c3945ad72f3cea04e41572fce13689d7ea77c
Annielle <64811906+anniellecrispim@users.noreply.github.com>
2024-06-17 18:03:12 -0300

    Delete nomeAlturaIdade_1.py
    deletando arquivo nomeAlturaIdade_1

--- nomeAlturaIdade_1.py
+++
@@ -1,5 +0,0 @@
-nomeUsuario = input("Digite seu nome: ")
-altura = float(input("Digite sua altura: "))
-idade = int(input("Digite sua idade: "))
-
-print(f"Seu nome é {nomeUsuario}, sua altura é {altura}m e você tem {idade} anos.")
