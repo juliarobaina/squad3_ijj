406b3fa580bf79e8c1d63bcb1b46ed707d7baf00
Vitor Back <vitor.back10@outlook.com>
2024-06-17 20:01:15 -0400

    exercicio operadores

---
+++ Exercici_Operacaaoteste.py
@@ -0,0 +1,3 @@
+valor = float(input("Insira um valor:"))
+
+print(f"O dobro é:{valor*2}\n o triplo do valor inserido é: {valor*3}\n o valor ao quadrado é: {valor**2}\n a raiz quadrada é: {valor**0.5}\n a raiz cubica é: {valor**(1/3)} ")
