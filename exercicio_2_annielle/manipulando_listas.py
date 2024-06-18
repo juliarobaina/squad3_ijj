bb4d4e459b9d89b01716a1af0ff609cb13def89c
annielle <anniellemedeiros@hotmail.com>
2024-06-18 17:40:09 -0300

    ajustando prints do primeiro e segundo cardápio

--- exercicio_2_annielle/manipulando_listas.py
+++ exercicio_2_annielle/manipulando_listas.py
@@ -2,3 +2,10 @@
 #Uma Lista em Python: []
 
 cardapio = ['sopa', 'macarronada', 'frango à milanesa', 'rubacão', 'guisado de bode', 'torta de frango', 'pernil de frango assado']
+
+print(cardapio)
+
+cardapio.remove('sopa')
+
+print(f'O cárdapio possui {len(cardapio)} pratos, sendo eles: {cardapio}')
+
