11fc5bc32f2d24192e790d3436374e02d44d5847
Vitor Back <vitor.back10@outlook.com>
2024-06-18 19:34:54 -0400

    -Adição de R$ -A maiusculo na pasta Aula 3

--- Aula-3/ExercicioFashion.py
+++ Aula-3/ExercicioFashion.py
@@ -3,11 +3,11 @@ valorDacompra = float(input("Informe o valor da compra para obter desconto:"))
 if valorDacompra > 500:
     print("PARABÉNS. VOCÊ GANHOU SUPER DESCONTO DE 30%")
     valordesconto = valorDacompra - (valorDacompra * 0.30) 
-    print(f"O valor da sua compra é: {valordesconto:.2f}")
+    print(f"O valor da sua compra é: R${valordesconto:.2f}")
 elif valorDacompra >= 250:
     print("PARABÉNS. VOCÊ GANHOU 10% DE DESCONTO, MAS PODE GANHAR 30% SE SUA COMPRA FOR ACIMA DE R$500,00")
     valordesconto = valorDacompra - (valorDacompra * 0.10) 
-    print(f"O valor da sua compra é: {valordesconto:.2f}")
+    print(f"O valor da sua compra é: R${valordesconto:.2f}")
 else:
     print("POXA, FALTA POUCO PARA VOCÊ GANHAR 10% DE DESCONTO EM SUA COMPRA.")
 
