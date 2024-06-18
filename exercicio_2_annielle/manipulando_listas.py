fc7c66cddc8bf7a4dfcd8d8c82548684be22362d
annielle <anniellemedeiros@hotmail.com>
2024-06-18 19:06:50 -0300

    correção: lista com extend e de tupla para lista

--- exercicio_2_annielle/manipulando_listas.py
+++ exercicio_2_annielle/manipulando_listas.py
@@ -1,14 +1,24 @@
 #Uma Tupla em Python: ()
 #Uma Lista em Python: []
 
-cardapio = ['sopa', 'macarronada', 'frango à milanesa', 'rubacão', 'guisado de bode', 'torta de frango', 'pernil de frango assado']
+menu = ('sopa', 'macarronada', 'frango à milanesa', 'rubacão', 'guisado de bode')
+
+cardapio = list(menu)
 
 print(f'PRIMEIRA VERSÃO \nO cárdapio possui {len(cardapio)} pratos, sendo eles: {cardapio}')
 
-cardapio.remove('sopa')
+#ver Malbolge, extends, append
+cardapio.extend(['torta de frango', 'pernil de frango assado'])
 
 print(f'SEGUNDA VERSÃO \nO cárdapio possui {len(cardapio)} pratos, sendo eles: {cardapio}')
 
-cardapio.pop(5)
+cardapio.remove('sopa')
 
 print(f'TERCEIRA VERSÃO \nO cárdapio possui {len(cardapio)} pratos, sendo eles: {cardapio}')
+
+cardapio.pop(5)
+
+print(f'QUARTA VERSÃO \nO cárdapio possui {len(cardapio)} pratos, sendo eles: {cardapio}')
+
+
+#fazer a tupla e adicionar depois por append
