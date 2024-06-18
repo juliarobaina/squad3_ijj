46af7993d4a7a21156fd090ac340d8e364655427
annielle <anniellemedeiros@hotmail.com>
2024-06-18 17:27:38 -0300

    trupla alternada para lista com mais 2 itens + organização de nome

---
+++ exercicio_2_annielle/manipulando_listas.py
@@ -0,0 +1,4 @@
+#Uma Tupla em Python: ()
+#Uma Lista em Python: []
+
+cardapio = ['sopa', 'macarronada', 'frango à milanesa', 'rubacão', 'guisado de bode', 'torta de frango', 'pernil de frango assado']
