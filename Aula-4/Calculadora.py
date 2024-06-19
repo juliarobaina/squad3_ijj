d4f2c245d81b7cf8cdd636dd8bc36c3b68e6cc46
Vitor Back <vitor.back10@outlook.com>
2024-06-19 18:49:21 -0400

    exercicio calculadora

---
+++ Aula-4/Calculadora.py
@@ -0,0 +1,35 @@
+print(f"\033[1m###################################\nOlá, Bem-vindo a CALUCLADORA DO IJJ\n###################################\033[0m")
+print("-------------------------------")
+
+while True:
+    print("Você pode escolher entre as seguintes operações:")
+    print("- Soma (+)")
+    print("- Subtração (-)")
+    print("- Multiplicação (*)")
+    print("- Divisão (/)")
+    print("- Potência (**)")
+
+    operacao = input("Qual operação deseja fazer:" ) 
+    numero1 = float (input("Informe o primeiro numero: "))
+    numero2 = float (input("Informe o segundo numero: "))
+
+    match operacao:
+
+        case '+':
+            print(f"{numero1} + {numero2} = {(numero1 +  numero2)}")
+        case '-':
+            print(f"{numero1} - {numero2} = {(numero1 - numero2)}")
+        case '*':
+            print(f"{numero1} * {numero2} = {(numero1 * numero2)}")
+        case '/':
+            print(f"{numero1} / {numero2} = {(numero1 / numero2)}")
+        case '**':
+            print(f"{numero1} ** {numero2} = {(numero1 ** numero2)}")
+        case _:
+            print("Operação inválida!")
+            
+    sair = input("Deseja continuar? s ou n ")
+
+    if sair == 'n':
+        break
+
