1c9368b2c7c8b723b3d2130f5f0f793ceb4bc78b
Tamires Ana <tamiresanap@outlook.com>
2024-06-30 19:07:17 -0300

    ajuste código dicionario_CEP

--- atividade_aula/Aula_VM/dicionario_CEP.py
+++ atividade_aula/Aula_VM/dicionario_CEP.py
@@ -1,16 +1,19 @@
 import requests
 
 # Dicionário com nome e CEP dos integrantes do squad
-squad = {
-    "Tamires": "07179260",
-    "Anielle": "58200000",
-    "Caio": "41194105",
-    "Vitor": "79009080",
-    "Julia": "25268160"
-}
+squad = [
+    {"nome": "Tamires", "cep": "07179260"},
+    {"nome": "Anielle", "cep": "58200000" },
+    {"nome": "Caio", "cep": "41194105"},
+    {"nome": "Vitor", "cep": "79009080"},
+    {"nome": "Julia", "cep": "25268160"}
+]
 
 # Itera sobre cada integrante do squad
-for name, cep in squad.items():
+for integrante in squad:
+    nome = integrante["nome"]
+    cep = integrante["cep"]
+
     # Faz a requisição para o serviço ViaCep
     response = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
     
@@ -18,7 +21,7 @@ for name, cep in squad.items():
     data = response.json()
     
     # Exibe os dados retornados
-    print(f'Nome: {name}')
+    print(f'Nome: {nome}')
     print(f'Localidade: {data["localidade"]}')
     print(f'CEP: {data["cep"]}')
     print('-' * 20)  # linha separadora entre os integrantes
