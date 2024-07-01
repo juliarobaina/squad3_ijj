1ddf5de2cc2d8a54ac39906753feb00182fbd687
annielle <anniellemedeiros@hotmail.com>
2024-07-01 20:23:15 -0300

    corrindo fuc de soma de números

--- annielle/desafios_da_aula/aula_x_funcao.py
+++ annielle/desafios_da_aula/aula_x_funcao.py
@@ -1,4 +1,13 @@
 def fraseDoDia():
     print(f'Lindo Dia')
 
+def soma(numeroA, numeroB):
+    soma = numeroA + numeroB
+    return soma
+
 fraseDoDia()
+numeroA = int(input("Digite um número "))
+numeroB = int(input("Digite outro número "))
+print(soma(numeroA,numeroB))
+
+
