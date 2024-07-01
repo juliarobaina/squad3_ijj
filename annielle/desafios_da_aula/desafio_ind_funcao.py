74d065837d184875a79e3f22a8decbd1ed48cb3a
annielle <anniellemedeiros@hotmail.com>
2024-07-01 20:36:31 -0300

    desafio do caique em função

---
+++ annielle/desafios_da_aula/desafio_ind_funcao.py
@@ -0,0 +1,12 @@
+def defineTime(matricula):
+    if matricula % 2 == 0:
+        return 'seu time é azul'
+    else: 
+        return 'seu time é amarelo'
+
+
+
+
+matricula = int(input("Digite um número "))
+
+print(defineTime(matricula))
