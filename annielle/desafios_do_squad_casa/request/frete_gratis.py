b40aa0f9dad864a0a009c467d85a9e0beaab25d6
annielle <anniellemedeiros@hotmail.com>
2024-07-02 15:57:14 -0300

    v1.1 com função

--- annielle/desafios_do_squad_casa/request/frete_gratis.py
+++ annielle/desafios_do_squad_casa/request/frete_gratis.py
@@ -2,12 +2,19 @@ import requests
 
 nordeste = {'MA', 'PI', 'CE', 'RN', 'PB', 'PE', 'AL', 'SE', 'BA'}
 norte = {'AC', 'AM', 'PA', 'RO', 'RR', 'TO'}
-cep = int(input('Qual é o cep?\n'))
+
+def fretegratuito (estado):
+    if estado in nordeste or estado in norte:
+        print('FRETE GRÁTIS')
+    else:
+        print('não é do estado do Nordeste ou Norte')
+
+cep = input('Qual é o cep?\n')
 
 response = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
 data = response.json()
 estado = data.get('uf')
-if estado in nordeste or norte:
-    print('FRETE GRÁTIS')
-else:
-    print('não é do estado do Nordeste ou Norte')
+
+fretegratuito(estado)
+annielle = 58200000
+julia = 25268160
