cb45a42ecbdf599c523644ebba01f0d07b042c0d
annielle <anniellemedeiros@hotmail.com>
2024-07-02 18:40:13 -0300

    desafio frete gratis para match case

--- annielle/desafios_do_squad_casa/request/frete_gratis.py
+++ annielle/desafios_do_squad_casa/request/frete_gratis.py
@@ -2,19 +2,37 @@ import requests
 
 nordeste = {'MA', 'PI', 'CE', 'RN', 'PB', 'PE', 'AL', 'SE', 'BA'}
 norte = {'AC', 'AM', 'PA', 'RO', 'RR', 'TO'}
+sudeste= {'ES', 'MG', 'SP', 'RJ'}
+centroOeste = {'GO', 'MS', 'MT'}
+sul = {'RS', 'PR', 'SC'}
 
-def fretegratuito (estado):
-    if estado in nordeste or estado in norte:
-        print('FRETE GRÁTIS')
-    else:
-        print('não é do estado do Nordeste ou Norte')
+def freteGratuito(valorCompra, estado):
+    match valorCompra:
+        case x if valorCompra >= 1 and estado in nordeste or estado in norte:
+            print('FRETE GRÁTIS POR LOCALIDADE')
+        case x if valorCompra > 50 and estado in sudeste or estado in sul:
+            print('FRETE GRÁTIS POR COMPRA')
+        case x if valorCompra > 80 and estado in centroOeste:
+            print('FRETE GRÁTIS POR COMPRA')
+        case _:
+            print('Não está elegível para frete grátis')
+            
 
 cep = input('Qual é o cep?\n')
+valorCompra = int(input('Quanto foi o valor da sua compra?'))
 
 response = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
 data = response.json()
 estado = data.get('uf')
 
-fretegratuito(estado)
+freteGratuito(valorCompra,estado)
+
+
+
+print(f'Status code', response.status_code)
+    
+
 annielle = 58200000
 julia = 25268160
+invalido = 60000000
+trativa = 60000-000 #tirar hifen e espaços 
