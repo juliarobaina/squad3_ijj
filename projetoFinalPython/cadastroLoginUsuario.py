c805024fccb88aa3104e78539d5a4b3eaf175423
juliarobaina <julia.robaina8@gmail.com>
2024-07-06 20:19:39 -0300

    trabalho final python com funções

--- projetoFinalPython/cadastroLoginUsuario.py
+++ projetoFinalPython/cadastroLoginUsuario.py
@@ -1,6 +1,32 @@
+def criarUsuarioAPI(usuario: dict ) -> None:
+    try:
+        response = requests.post('https://desafiopython.jogajuntoinstituto.org/api/users/', data=usuario)
+        #uma requisição malfeita (resposta que não tenha código 200), levanta uma exceção
+        response.raise_for_status()
+        print(f"Usuário {usuario['username']} criado com sucesso!")
+    #pega erros HTTP
+    except requests.exceptions.HTTPError as error:
+        print("Erro:", error)
+
+def salvarRespostaJson(response: dict) -> None:
+    with open('respostaLoginUsuario.txt','w') as file:
+        for key,value in response.items():
+            file.write(key + ': ')
+            file.write(value + '\n')
+
+def logarUsuarioAPI(email: str, senha: str, nomeUsuario: str) -> None:
+    try:
+        response = requests.post('https://desafiopython.jogajuntoinstituto.org/api/users/login/', data={'email':email,'password':senha})
+        response.raise_for_status()
+        print(f"Usuário {nomeUsuario} logado com sucesso!")
+        salvarRespostaJson(response.json())
+    except requests.exceptions.HTTPError as error:
+        print("Erro:", error)
+
 import requests
 from faker import Faker
 
+#gerar dados falsos para ter um usuário falso
 fake = Faker('pt_BR')
 usuario = {
     'username': fake.user_name(),
@@ -8,10 +34,10 @@ usuario = {
     'password': fake.password(),
     'phone': fake.phone_number(),
     'address': fake.address(),
-    'cpf': fake.cpf()
-    
+    'cpf': fake.cpf()  
 }
 
+#para substituir a quebra de linha do endereço criado pela biblioteca faker por um hífen
 splitAddress = usuario['address'].split('\n')
 usuario['address'] = " - ".join(splitAddress)
 
@@ -19,36 +45,6 @@ print('Os dados do usuário criado foram')
 for key,value in usuario.items():
     print(f'{key}: {value}')
 
-urlCriarUsuario = 'https://desafiopython.jogajuntoinstituto.org/api/users/'
+criarUsuarioAPI(usuario)
 
-try:
-    response = requests.post(urlCriarUsuario, data=usuario)
-    print(response.raise_for_status())
-    print(f"Usuário {usuario['username']} criado com sucesso!")
-
-except requests.exceptions.HTTPError as error:
-    print("Erro:", error)
-
-login = {
-    'email': usuario['email'],
-    'password': usuario['password']
-}
-urlLoginUsuario = 'https://desafiopython.jogajuntoinstituto.org/api/users/login/'
-
-try:
-    response = requests.post(urlLoginUsuario, data=login)
-    response.raise_for_status()
-    print(f"Usuário {usuario['username']} logado com sucesso!")
-
-except requests.exceptions.HTTPError as error:
-    print("Erro:", error)
-
-with open('respostaLoginUsuario.txt','w') as file:
-    for key,value in response.json().items():
-        file.write(key + ': ')
-        file.write(value + '\n')
-
-#remover quebra de linha
-#cadastrar usuário
-#logar usuário
-#salvar login json no arquivo
+logarUsuarioAPI(usuario['email'],usuario['password'],usuario['username'])
